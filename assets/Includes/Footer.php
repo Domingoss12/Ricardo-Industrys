776bbde759ef30dd293e715d81a18410bab254de
Ricardo Domingos <ricardomgdomingos@gmail.com>
2023-09-21 10:35:12 +0100

    Build 1.0.2

---
+++ assets/Includes/Footer.php
@@ -0,0 +1,69 @@
+<!-- Footer Desktop Start-->
+<footer class="hidden md:block p-4 bg-gray-100 rounded-lg shadow px-6 py-8 dark:bg-gray-900">
+    <div class="flex items-center justify-between">
+        <a href="../index.php" class="flex items-center mb-0">
+            <img src="./assets/Images/Icon.png" class="h-8 mr-3" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ricardo Industry's</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 dark:text-gray-400">
+            <li>
+                <a href="minhaconta.php" class="mr-4 hover:underline md:mr-6 ">Minha Conta</a>
+            </li>
+            <li>
+                <a href="encomendas.php" class="mr-4 hover:underline md:mr-6">Encomendas</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="my-6 border-gray-200 mx-auto dark:border-gray-700 lg:my-8" />
+    <div class="flex items-center justify-between">
+        <span class="text-sm text-gray-500 text-center dark:text-gray-400">© 2023 <a href="../index.php" class="hover:underline">Ricardo Industry's</a>. Todos os Direitos Reservados.</span>
+        <div class="flex mt-4 space-x-6 justify-center :mt-0">
+            <a href="https://github.com/Domingoss12" class="text-gray-500 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-github w-5 h-5" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+            </a>
+            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-linkedin w-5 h-5" viewBox="0 0 16 16">
+                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                </svg>
+            </a>
+        </div>
+    </div>
+</footer>
+
+<!-- Footer Desktop End -->
+<!-- Footer Mobile Start -->
+<footer class="md:hidden p-4 bg-white rounded-lg shadow dark:bg-gray-900">
+    <div>
+        <a href="index.php" class="flex items-center mb-4">
+            <img src="./assets/Images/Icon.png" class="h-8 mr-3" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Ricardo Industry's</span>
+        </a>
+        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 dark:text-gray-400">
+            <li>
+                <a href="minhaconta.php" class="mr-4 hover:underline">Minha Conta</a>
+            </li>
+            <li>
+                <a href="encomendas.php" class="mr-4 hover:underline">Encomendas</a>
+            </li>
+        </ul>
+    </div>
+    <hr class="my-6 border-gray-200 dark:border-gray-700 lg:my-8" />
+    <div>
+        <span class="text-sm text-gray-500 dark:text-gray-400">© 2023 <a href="index.php" class="hover:underline">Ricardo Industry'<s></s></a>. Todos os Direitos Reservados.</span>
+        <div class="flex mt-4 space-x-6 ">
+            <a href="https://github.com/Domingoss12" class="text-gray-500 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-github w-5 h-5" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                </svg>
+            </a>
+            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white" target="_blank">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-linkedin w-5 h-5" viewBox="0 0 16 16">
+                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+                </svg>
+            </a>
+        </div>
+    </div>
+</footer>
+<!-- Footer Mobile End -->
--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css" rel="stylesheet" />
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <!-- Local Host -->
+    <link rel="stylesheet" href="assets/css/styles.css">
 
 </head>
 
@@ -64,53 +66,86 @@
             </button>
         </div>
 
-        <div class="bg-gray-50 dark:bg-gray-700">
-            <h1 class="text-4xl font-medium text-gray-900 dark:text-white">Destaques</h1>
+        <!-- Destaques -->
+        <div class="p-5">
+            <h1 class="text-4xl font-bold text-gray-900 dark:text-white p-2">Destaques</h1>
+            <div class="media-scroller snaps-inline">
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?><?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
 
 
-            <div id="controls-carousel" class="relative w-full" data-carousel="static">
-                <!-- Carousel wrapper -->
-                <div class="relative h-full overflow-hidden rounded-lg">
-                    <!-- Item 1 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                        <div class="grid grid-cols-4 space-x-1 p-2">
-
-                            <?php include "assets/Includes/card.php" ?>
-                            <?php include "assets/Includes/card.php" ?>
-                            <?php include "assets/Includes/card.php" ?>
-                            <?php include "assets/Includes/card.php" ?>
-                        </div>
-                    </div>
-                    <!-- Item 2 -->
-                    <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
-                        <img src="/docs/images/carousel/carousel-2.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-                    </div>
-                </div>
-                <!-- Slider controls -->
-                <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                        </svg>
-                        <span class="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                        </svg>
-                        <span class="sr-only">Next</span>
-                    </span>
-                </button>
             </div>
+        </div>
+
+        <!-- Promocoes -->
+        <div class="p-5">
+            <h1 class="text-4xl font-bold text-gray-900 dark:text-white p-2">Promoções</h1>
+            <div class="media-scroller snaps-inline">
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?><?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
 
 
+            </div>
+        </div>
+
+        <!-- Top Vendas -->
+        <div class="p-5">
+            <h1 class="text-4xl font-bold text-gray-900 dark:text-white p-2">Top Vendas</h1>
+            <div class="media-scroller snaps-inline">
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?><?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+                <?php include "assets/Includes/cardProduct.php" ?>
+
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Footer Start -->
+    <div class="bg-gray-100 border-gray-200 dark:bg-gray-900">
+        <div class="max-w-screen-2xl mx-auto">
+            <?php include_once "assets/includes/footer.php" ?>
         </div>
     </div>
 
     <!-- Script -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
-</body>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
 
-</html><?php include "assets/Includes/card.php" ?>
+</body>
