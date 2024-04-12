273de725757fe0c72db0fd2e8c4718315ed09a84
Ricardo Domingos <ricardomgdomingos@gmail.com>
2024-04-12 03:36:34 +0100

    UPT

--- criarConta.php
+++ criarConta.php
@@ -1,3 +1,16 @@
+<?php
+include "assets/class/reCaptchaVerification.class.php";
+include "assets/class/variablesPublic.class.php";
+
+
+if ($_POST) {
+
+    echo reCAPTCHA_Validation($_POST['g-recaptcha-response']);
+
+    return;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-PT">
 
@@ -19,11 +32,11 @@
 
 <body class="bg-[url('assets/Images/Background.svg')] bg-repeat">
     <div class="hidden md:flex items-center justify-center h-screen">
-        <div class="w-2/6 h-3/5 bg-gray-100 dark:bg-gray-800 rounded-xl shadow-2xl">
+        <div class="w-2/6 pb-10 pt-2 bg-gray-100 dark:bg-gray-800 rounded-xl shadow-2xl">
             <div class="flex items-center">
                 <a href="index.php" class="flex items-center mx-auto m-4">
                     <img src="assets/Images/Icon.png" class="mr-3 h-6 sm:h-9" />
-                    <span class="self-center md:text-2xl font-bold whitespace-nowrap dark:text-white">Ricardo Industry's</span>
+                    <span class="self-center md:text-3xl font-black whitespace-nowrap dark:text-white">Ricardo Industry's</span>
                 </a>
             </div>
             <div class="h-3/4 flex items-center justify-center">
@@ -40,6 +53,9 @@
                         <label for="reapeatPassword" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmar palavra-passe</label>
                         <input type="password" id="reapeatPassword" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-500 focus:border-cyan-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-cyan-500 dark:focus:border-cyan-500" placeholder="••••••••" required>
                     </div>
+                    <div class="flex justify-center	itens-center mb-5">
+                        <div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>"></div>
+                    </div>
                     <div class="w-full">
                         <p class="text-gray-500 dark:text-gray-400 text-center">Já tens uma conta? Clica <a href="login.php" class="font-medium text-cyan-600 underline dark:text-cyan-500 hover:no-underline">aqui</a> para iniciares sessão na tua conta!</p>
                     </div>
@@ -54,6 +70,7 @@
     <!-- Script -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </body>
 
 </html>
